3155695a08e898c8ef5357cc98db88c448c473d4
Mushfiqur719 <rahmanmushfiqur719@gmail.com>
2021-12-08 18:53:40 +0600

    2: Added room,message Models

--- base/models.py
+++ base/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth import User
 
 # Create your models here.
+
+class Room(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True,blank=True)
+
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+class Message(models.Model):
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    body = models.TextField()
+    updated = models.DateTimeField(auto_now = True)
+    created = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        return self.body[0:50]
--- base/views.py
+++ base/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Room
 
 # Create your views here.
 
@@ -10,14 +11,12 @@ rooms=[
 
 
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render(request, 'base/home.html',context)
 
 
 def room(request,pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    room = Room.objects.get(id=pk)
     context = {'room': room}
     return render(request, 'base/room.html',context)
