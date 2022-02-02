9cae63522934eb623fb1c1d98c6f8474e10293db
Mushfiqur719 <rahmanmushfiqur719@gmail.com>
2022-02-02 20:32:03 +0600

    4: Searching functionality, left navbar, searchbar

--- base/views.py
+++ base/views.py
@@ -1,19 +1,30 @@
 from django.shortcuts import render,redirect
-from .models import Room
+from django.db.models import Q
+from .models import Room,Topic
 from .forms import RoomForm
 
 # Create your views here.
 
-rooms=[
-    {'id' : 1, 'name': 'Lets Learn Python!'},
-    {'id' : 2, 'name': 'Lets Learn DSA!'},
-    {'id' : 3, 'name': 'Lets Learn Django!'},
-]
+# rooms=[
+#     {'id' : 1, 'name': 'Lets Learn Python!'},
+#     {'id' : 2, 'name': 'Lets Learn DSA!'},
+#     {'id' : 3, 'name': 'Lets Learn Django!'},
+# ]
 
 
 def home(request):
-    rooms = Room.objects.all()
-    context = {'rooms': rooms}
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains = q) |
+        Q(name__icontains = q) |
+        Q(description__icontains = q)
+    )
+
+    topics = Topic.objects.all()
+
+    context = {'rooms': rooms, 'topics':topics}
+    
     return render(request, 'base/home.html',context)
 
 
